bf5bee2df16d806e32e11f0152829e08ba90db56
kago1112 <kasiaagolawskaa@gmail.com>
2025-05-23 16:09:54 +0200

    odswiezenie

--- T10/MeritoWebApp/obj/Debug/net9.0/MeritoWebApp.AssemblyInfo.cs
+++ T10/MeritoWebApp/obj/Debug/net9.0/MeritoWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeritoWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26f9c4520dce777a437172c44444c4c17f3df7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f787f08b247ff4ff18521594f5c1c981348cbc")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeritoWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeritoWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ T4/1_ulubiona_ksiazka.py
@@ -0,0 +1,7 @@
+# CWICZENIE 1
+# Napisz funkcję o nazwie ulubiona_ksiazka(), która akceptuje jeden parametr - tytul. Funkcja powinna wydrukować wiadomość, taką jak Moja ulubiona ksiazka jest Hobbit . Wywołaj funkcję, upewniając się, że tytuł książki jest podany jako argument w wywołaniu funkcji.
+
+def ulubiona_ksiazka(tytul):
+    print(f"Moja ulubiona książka jest {tytul}")
+
+ulubiona_ksiazka("Hobbit")
